48a505683714eebc6c98f845e1fe17ef946e9ce1
Steven Gong <gonghaima@hotmail.com>
2024-05-01 20:35:50 +1000

    add css and tailwind

--- app/CollaborativeApp.tsx
+++ app/CollaborativeApp.tsx
@@ -2,7 +2,7 @@
 
 import { useOthers } from '../liveblocks.config';
 
-export function CollaborativeApp() {
+export const CollaborativeApp = () => {
   const others = useOthers();
   const userCount = others.length;
   return <div>There are {userCount} other user(s) online</div>;
