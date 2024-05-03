604ae8ad2e8c5f0a83bd5e795d088ab3196f9efe
Steven Gong <gonghaima@hotmail.com>
2024-05-03 17:49:34 +1000

    layout update - 21:20 progress

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
   return (
-    <Room>
       <h1 className='font-xl'>Liveblock Figma Clone</h1>
-    </Room>
   );
 }
