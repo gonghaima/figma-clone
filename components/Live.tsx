23a9f04bc84b96dd4e6109df36711594559b3cda
Steven Gong <gonghaima@hotmail.com>
2024-05-07 10:47:52 +1000

    ReactionSelector feature

--- components/Live.tsx
+++ components/Live.tsx
@@ -3,17 +3,27 @@ import { useMyPresence, useOthers } from '@/liveblocks.config';
 import LiveCursors from './cursor/LiveCursors'
 import CursorChat from './cursor/CursorChat'
 import { CursorMode, CursorState, Reaction, ReactionEvent } from "@/types/type";
+import ReactionSelector from './reaction/ReactionButton';
 
 
 function Live() {
     const others = useOthers();
     const [{ cursor }, updateMyPresence] = useMyPresence() as any;
 
+    // store the reactions created on mouse click
+    // const [reactions, setReactions] = useState<Reaction[]>([]);
+
     // track the state of the cursor (hidden, chat, reaction, reaction selector)
     const [cursorState, setCursorState] = useState<CursorState>({
         mode: CursorMode.Hidden,
     });
 
+    // set the reaction of the cursor
+    const setReactions = useCallback((reaction: string) => {
+        setCursorState({ mode: CursorMode.Reaction, reaction, isPressed: false });
+    }, []);
+
+    // const setReactions = useCallback((reaction: ReactionEvent) => {});
 
     // Listen to mouse events to change the cursor state
     const handlePointerMove = useCallback((event: React.PointerEvent) => {
@@ -47,22 +57,34 @@ function Live() {
         }
     }, []);
 
-    const handlePointerDown = useCallback((event: React.PointerEvent) => {
-        // if cursor is not in reaction selector mode, update the cursor position
-        if (cursor == null || cursorState.mode !== CursorMode.ReactionSelector) {
+    // Show the cursor when the mouse enters the canvas
+    const handlePointerDown = useCallback(
+        (event: React.PointerEvent) => {
             // get the cursor position in the canvas
             const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
             const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
 
-            // broadcast the cursor position to other users
             updateMyPresence({
                 cursor: {
                     x,
                     y,
                 },
             });
-        }
-    }, []);
+
+            // if cursor is in reaction mode, set isPressed to true
+            setCursorState((state: CursorState) =>
+                cursorState.mode === CursorMode.Reaction ? { ...state, isPressed: true } : state
+            );
+        },
+        [cursorState.mode, updateMyPresence, setCursorState]
+    );
+
+    // hide the cursor when the mouse is up
+    const handlePointerUp = useCallback(() => {
+        setCursorState((state: CursorState) =>
+            cursorState.mode === CursorMode.Reaction ? { ...state, isPressed: false } : state
+        );
+    }, [cursorState.mode, setCursorState]);
 
     // Listen to keyboard events to change the cursor state
     useEffect(() => {
@@ -101,6 +123,7 @@ function Live() {
             onPointerMove={handlePointerMove}
             onPointerLeave={handlePointerLeave}
             onPointerDown={handlePointerDown}
+            onPointerUp={handlePointerUp}
             className="h-[100vh] w-full flex justify-center items-center"
         >
             <h1 className='text-2xl text-white'>Liveblock Figma Clone</h1>
@@ -110,6 +133,12 @@ function Live() {
                     cursorState={cursorState}
                     setCursorState={setCursorState}
                     updateMyPresence={updateMyPresence}
+                />
+            )}
+            {/* If cursor is in reaction selector mode, show the reaction selector */}
+            {cursorState.mode === CursorMode.ReactionSelector && (
+                <ReactionSelector
+                    setReaction={setReactions}
                 />
             )}
             <LiveCursors others={others} />
