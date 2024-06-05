33fb0d5e379c83371d8b6143b4d62e749bf125c4
Steven Gong <gonghaima@hotmail.com>
2024-06-06 00:35:55 +1000

    chat

--- components/Live.tsx
+++ components/Live.tsx
@@ -6,6 +6,7 @@ import { CursorMode, CursorState, Reaction, ReactionEvent } from "@/types/type";
 import ReactionSelector from './reaction/ReactionButton';
 import FlyingReaction from './reaction/FlyingReaction';
 import useInterval from '@/hooks/useInterval';
+import { Comments } from './comments/Comments';
 
 type Props = {
     canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
@@ -240,6 +241,7 @@ const Live = ({ canvasRef, undo, redo }: Props) => {
                 />
             )}
             <LiveCursors others={others} />
+            <Comments/>
         </div>
     )
 }
